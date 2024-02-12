f11b59e70d4ac5a75272a2d501af76c89651ae6e
sabbir1452 <sabbirrahman1452@gmail.com>
2024-02-11 20:03:54 -0800

    adjfj

---
+++ practice.js
@@ -0,0 +1,24 @@
+/* 
+JS concepts:
+1. null vs undefined
+2. == vs ===
+3. scope: block scope, function scope, global scope
+4. closure
+5. hoisting
+6. callback function
+7. arguments. what is array like object.
+8. array vs array like object
+9. turthy vs flasy
+10. ! vs !!
+*/
+
+/* 
+Dev tool
+1. Elements tab
+2. Console tab
+3. Source 
+4. Network
+5. Application
+6. Check Responsive
+7. Shourtcut keys
+*/
